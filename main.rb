ac4bdcd7648216b389e96b4c1ac7036f3435f3d2
analuciachin <ananime@gmail.com>
2021-02-01 21:08:28 +0000

    code cleaning

--- main.rb
+++ main.rb
@@ -1,16 +1,8 @@
 require './player'
 require './questionSet'
 
-# player1 = Player.new("player1")
-# puts player1.name
 
 class Game
-
-  def initialize
-    @player1 = Player.new("Player 1")
-    @player2 = Player.new("Player 2")
-  end
-
 
   def generateQuestion
     num1 = rand(0..20)
@@ -19,71 +11,67 @@ class Game
     question
   end
 
-
-  def isAnswerCorrect playerAnswer
-    if playerAnswer == question.answer
-      true
-    else false
-    end
-  end
-
   
   def play
+
+    player1 = Player.new("Player 1")
+    player2 = Player.new("Player 2")
     scorePlayerOne = 0
     scorePlayerTwo = 0
     isPlayerOneTurn = true
 
     while scorePlayerOne < 3 && scorePlayerTwo < 3
+      puts "--- NEW TURN ---"
       if isPlayerOneTurn == true
         question = generateQuestion()
-        puts question.question
+        puts "#{player1.name}: #{question.question}"
+        #print question.question
         playerAnswer = $stdin.gets.chomp
 
         if playerAnswer == question.answer
-          puts "Yes! You are correct."
+          puts "#{player1.name}: Yes! You are correct."
           scorePlayerOne = scorePlayerOne + 1
           isPlayerOneTurn = false
+          puts "P1: #{scorePlayerOne}/3 vs P2: #{scorePlayerTwo}/3"
         else 
-          puts "Seriously? No!"
+          puts "#{player1.name}: Seriously? No!"
           isPlayerOneTurn = false
+          puts "P1: #{scorePlayerOne}/3 vs P2: #{scorePlayerTwo}/3"
         end
 
       end
 
+      puts "--- NEW TURN ---"
       if isPlayerOneTurn == false
         question = generateQuestion()
-        puts question.question
+        puts "#{player2.name}: #{question.question}"
         playerAnswer = $stdin.gets.chomp
 
         if playerAnswer == question.answer
-          puts "Yes! You are correct."
+          puts "#{player2.name}: Yes! You are correct."
           scorePlayerTwo = scorePlayerTwo + 1 
           isPlayerOneTurn = true
+          puts "P1: #{scorePlayerOne}/3 vs P2: #{scorePlayerTwo}/3"
         else 
-          puts "Seriously? No!"
+          puts "#{player2.name}: Seriously? No!"
           isPlayerOneTurn = true
+          puts "P1: #{scorePlayerOne}/3 vs P2: #{scorePlayerTwo}/3"
         end
 
       end
 
     end
 
+    puts "--- GAME OVER ---"
+    puts "Good bye!"
     exit(0)
 
   end
 
 end
 
-# num1 = rand(0..20)
-# num2 = rand(0..20)
-# newQuestion = QuestionSet.new("What does #{num1} plus #{num2} equal?", "#{num1+num2}")
-# puts newQuestion.question
-# puts newQuestion.answer
-
 
 game = Game.new
-# puts game.generateQuestion.answer
-# puts game.generateQuestion.question
 puts game.play
 
 
